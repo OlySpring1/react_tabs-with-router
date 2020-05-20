ec084abe16919d6292232eb43e562126e32420d4
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-05-20 22:44:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8758f7d299f4d9363f87977cceac2196.js"
+  "./precache-manifest.e8653f1fa9676734ca81fff0393545cb.js"
 );
 
 self.addEventListener('message', (event) => {
